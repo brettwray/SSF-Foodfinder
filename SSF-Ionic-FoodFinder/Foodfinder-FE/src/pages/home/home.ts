262b225504ba55869cff78dba6334434fe3e3bf7
BrettWray <brett@brettwray.com>
2018-10-15 00:22:10 -0700

    moved geolocation to a service, added Google Maps

--- SSF-Ionic-FoodFinder/Foodfinder-FE/src/pages/home/home.ts
+++ SSF-Ionic-FoodFinder/Foodfinder-FE/src/pages/home/home.ts
@@ -12,32 +12,16 @@ declare var google;
 })
 export class HomePage {
 
-lat;
-lng;
+
 
 
   constructor(public navCtrl: NavController, public LOCATE: LocateProvider, public storage: Storage) {
-
-  }
- async getLocationData() {
-  this.lat = await this.storage.get('lat');
-  this.lng = await this.storage.get('lng');
-  this.drawMap(this.lat, this.lng)
+    this.LOCATE.mapData();
   }
 
-async drawMap(latitude, longitude) {
-let currentPos = {lat: await latitude, lng: await longitude};
-
-let mapOptions = {
-  zoom: 15,
-  center: currentPos
-}
-let map = new google.maps.Map(document.getElementById('map'), mapOptions)
-let marker = new google.maps.Marker({position: currentPos, map: map})
-}
 
 ionViewDidLoad(){
-this.getLocationData()
+  this.LOCATE.drawMap();
 }
 
 }
--- SSF-Ionic-FoodFinder/Foodfinder-FE/src/providers/locate/locate.ts
+++ SSF-Ionic-FoodFinder/Foodfinder-FE/src/providers/locate/locate.ts
@@ -8,6 +8,11 @@ declare var google;
 @Injectable()
 export class LocateProvider {
 
+  map: any;
+  marker: any;
+  service: any;
+  infoWindow: any;
+  placeMarker:any;
 constructor(public http: HttpClient, public geolocation: Geolocation, public storage: Storage) {}
   
 getPosition() {
@@ -18,6 +23,41 @@ getPosition() {
     }).catch((error) =>{
       console.log('locateProvider - getCurrentPosition', error)
     })
-  } 
+  }
+async mapData() {
+  let currentPos = {lat: await this.storage.get('lat'), lng: await this.storage.get('lng')}
+
+  let mapOptions = {
+    zoom: 15,
+    center: currentPos
+  }
+  this.map = new google.maps.Map(document.getElementById('map'), mapOptions)
+  this.marker = new google.maps.Marker({position: currentPos, map: this.map})
+
+  this.service = new google.maps.places.PlacesService(this.map);
+  this.service.nearbySearch({
+    location: currentPos,
+    radius: 500,
+    type:['restaurant']
+  }, this.placeSearch);
 }
+  async placeSearch(results, status){
+    console.log('place search ran', results, status)
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      let rand = results[(Math.random() * results.length) | 0]
+      this.placeMarker = new google.maps.marker({
+        map: await this.map,
+        position: rand.geometry.location
+      })
+    }
+  }
+
+async drawMap(){
+  await this.map;
+  await this.marker;
+  
+  await this.placeMarker;
+}
+} 
+
 
